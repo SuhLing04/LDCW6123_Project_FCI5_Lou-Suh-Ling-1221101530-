e1aa90cb306655c7b74045178d77566db9f42e47
yiwenyw <yongyiwen1221@gmail.com>
2026-09-23 13:47:50 +0800

    Created fees.h

---
+++ fees.h
@@ -0,0 +1,5 @@
+// fees.h - Delivery fee, surcharge, service fee, delivery time (owner: C2)
+#ifndef FEES_H
+#define FEES_H
+
+const double MAX_DISTANCE_KM = 20.0;   // we do not deliver beyond this
