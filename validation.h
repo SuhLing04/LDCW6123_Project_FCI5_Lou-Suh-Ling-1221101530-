78c1faab10f8f3468a611c1607274a831860e1ac
yiwenyw <yongyiwen1221@gmail.com>
2026-09-23 23:02:48 +0800

    Added prototypes readInt, readDouble, readYesNo, readLine

--- validation.h
+++ validation.h
@@ -4,4 +4,17 @@
 
 #include <string>
 
+// keep asking until the user enters a whole number between minVal and maxVal
+int readInt(const std::string& prompt, int minVal, int maxVal);
+
+// keep asking until the user enters a number between minVal and maxVal
+double readDouble(const std::string& prompt, double minVal, double maxVal);
+
+// keep asking until the user enters y/yes or n/no
+// return true for yes
+bool readYesNo(const std::string& prompt);
+
+// read a whole line of text (may be empty)
+std::string readLine(const std::string& prompt);
+
 #endif
