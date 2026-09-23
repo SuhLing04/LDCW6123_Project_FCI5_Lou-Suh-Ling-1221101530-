6e2c9e732c6336100f410d17b8c2c3608cb38e7d
yiwenyw <yongyiwen1221@gmail.com>
2026-09-23 22:11:23 +0800

    promo.cpp file

---
+++ promo.cpp
@@ -0,0 +1,10 @@
+// promo.cpp - Promo code logic (owner: C2)
+
+// Winston model link: promo codes and discounts are how platforms pushed
+// DIFFUSION (getting many users to adopt the service quickly)
+
+#include "promo.h"
+#include <cctype>
+#include <cmath>
+
+using namespace std;
