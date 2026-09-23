156181ef8d98794e8c3850a640cd76b0feed0dbc
yiwenyw <yongyiwen1221@gmail.com>
2026-09-23 21:48:48 +0800

    add PromoResult struct to store disc details

--- promo.h
+++ promo.h
@@ -3,3 +3,9 @@
 #define PROMO_H
 
 #include <string>
+
+struct PromoResult {
+    bool valid;            // true if the code was applied
+    double discount;       // amount taken off (RM)
+    std::string message;   // text to show the user
+};
